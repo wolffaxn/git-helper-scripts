f4adcdf4adcb334fd299a9082ae7609ae945fe3e
Alexander Wolff <wolffaxn@gmail.com>
2019-08-26 22:28:47 +0200

    add zsh plugin

---
+++ git-helper-scripts.plugin.zsh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2019 Alexander Wolff <wolffaxn[at]gmail[dot]com>
+
+# add plugin's bin diretory to user's path
+PLUGIN_PATH=$(dirname "$0")
+export PATH="${PATH}:${PLUGIN_PATH}/bin"
+
+zstyle ':completion:*:*:git:*' user-commands \
+  sync:'Sync repository with forked repository' \
+  update:'Update remote repository'
